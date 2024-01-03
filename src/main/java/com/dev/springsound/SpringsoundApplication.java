8fee6033ca53ec5c389acc5422d03c54a2ee0b1f
Felipe <felipecastro0208@hotmail.com>
2024-01-02 21:47:32 -0300

    Initial project with default configs

---
+++ src/main/java/com/dev/springsound/SpringsoundApplication.java
@@ -0,0 +1,20 @@
+package com.dev.springsound;
+
+import com.dev.springsound.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsoundApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsoundApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+	}
+}
---
+++ src/test/java/com/dev/springsound/SpringsoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.springsound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
